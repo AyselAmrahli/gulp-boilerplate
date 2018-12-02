7af1affe646e4b600053890cb26d9cc286fc8e63
AyselAmrahli <ayselamrahli.com>
2018-12-02 23:53:05 +0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+let gulp = require('gulp');
+let webserver = require('gulp-webserver');
+let cleanCSS = require('gulp-clean-css');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let sourcemaps = require('gulp-sourcemaps');
+let handlebars = require('gulp-compile-handlebars');
+let rename = require('gulp-rename');
+
+let sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+
+
+// *************task for sass to css*****************//
+gulp.task('css', function () {
+  return gulp.src('dev/sass/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS()) 
+    // .pipe(rename('styles.min.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/css'), { sourcemaps: true });
+});
+
+
+// ************** task for  js minify and concat*******//
+gulp.task('scripts', function() {
+    return gulp.src('dev/js/**/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(rename('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets/js'));
+});
+
+
+// ****************task for hbs to html***************//
+gulp.task('html', function () { 
+    return gulp.src('dev/templates/pages/**.hbs')
+        .pipe(handlebars({}, {
+          ignorePartials: true,
+          batch: ['dev/templates/partials']
+        }))
+        .pipe(rename({
+          extname: '.html'
+        }))
+        .pipe(gulp.dest('public'));
+});
+
+
+
+// ****************task for watch******************//
+gulp.task('watch', ['css','scripts', 'html'], function (){
+  gulp.watch('dev/sass/**/**.scss', ['css']);
+  gulp.watch('dev/js/**/**.js', ['scripts']);
+  gulp.watch('dev/templates/**/**.hbs', ['html']);
+});
+
+
+
+// ******************gulp serve*****************//
+gulp.task('serve', ['watch'], function () {
+  return gulp.src('public')
+    .pipe(webserver({
+      port: 3000,
+      livereload: true,
+      fallback: 'index.html'
+    }));
+});
